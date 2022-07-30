fca15db63692cb0f55194ba13e6a29c953c16e40
alirezaamini <ali.amini83@gmail.com>
2022-07-30 14:54:50 +0200

    Changes to be committed: 	modified:   main.py 	new file:   modules/helpers.py
    Untracked files:
    	.gitignore

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+import modules.helpers as hp
 
 print('test', '55g', 25, sep=f'\n{50*"^"}\n')
 
+x, y = 20.5, 30.5
+res = hp.summation(x, y)
+print(res)
---
+++ modules/helpers.py
@@ -0,0 +1,3 @@
+def summation(a: float, b: float):
+    c = a + b
+    return c
